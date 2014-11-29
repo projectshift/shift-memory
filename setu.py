5f9a8965ce96e3fc16a3fa5f61abd26c0d92844f
Dmitry Belyakov <db@indee.ru>
2014-11-29 17:17:49 +0300

    License and setup updates

--- setu.py
+++ setu.py
@@ -9,8 +9,13 @@ setup(
     # project meta
     name = 'shiftmemory',
     version='0.0.1',
+    url = 'https://github.com/projectshift/shift-memory',
+    download_url='https://github.com/projectshift/shift-memory/tarball/0.0.1',
     description='Python3 cache library',
-    keywords='python3, cache, redis, memcached',
+    keywords=['python3', 'cache', 'redis', 'memcached'],
+    classfiers=['python3', 'cache', 'redis', 'memcached'],
+
+    # license
     license = 'MIT',
 
     # package modules
